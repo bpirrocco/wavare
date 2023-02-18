15f92623c2d9a398b66dad83a021287d53982b46
Brandon Pirrocco <bpirrocco@gmail.com>
2023-02-18 10:37:44 -0500

    Redirect fix using JS

--- home/static/home/script.js
+++ home/static/home/script.js
@@ -1,7 +1,8 @@
-let nazare = document.querySelector('.nazare')
-let nazButton = document.querySelector('.nazbutton')
+let nazare = document.querySelector('#nazare');
+let nazButton = document.querySelector('#nazbutton');
 
 
 nazButton.addEventListener("click", function() {
-    location.replace(`127.0.0.1:8000/forecast/1/2023-02-10/hourly`)
+    nazare = nazare.value;
+    window.location.href = `1/2023-02-10/${nazare}`
 })
