f078d80aed7ecde0369e1b5c7b23f485a3e67f41
Brandon Pirrocco <bpirrocco@gmail.com>
2023-02-19 11:41:24 -0500

    Add contact to urls.py

--- wavare/urls.py
+++ wavare/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, register_converter
 
-from home.views import welcome, nazare, forecast, locations, QuerystringRedirect
+from home.views import welcome, nazare, forecast, locations, QuerystringRedirect, contact
 from home import converters
 
 register_converter(converters.DateConverter, 'date')
@@ -29,4 +29,5 @@ urlpatterns = [
     path('forecast/', locations, name="location"),
     # re_path(r'^forecast/$', QuerystringRedirect.as_view(), name="forecaster"),
     # path('location/<int:id>', location, name="location")
+    path('contact', contact, name="contact"),
 ]
