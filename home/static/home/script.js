cdcde62fa957409fb86e1d3169864cc8e626ea40
Brandon Pirrocco <bpirrocco@gmail.com>
2023-02-18 20:06:52 -0500

    TODO's

--- home/static/home/script.js
+++ home/static/home/script.js
@@ -7,19 +7,32 @@
 //     window.location.href = `1/2023-02-10/${nazare}`
 // })
 
-let location, interval;
+// let location, interval;
 
-function getInfo(e) {
+function getInfo(e, location, interval) {
     let button, selector;
     button = e.target;
     selector = button.previousSibling;
-    location = selector.id;
+    location = selector.parentNode.id;
     interval = selector.value;
+    return location, interval;
 }
 
-let buttons = document.querySelector('button');
-
-buttons.addEventListener("click", function(e) {
-    getInfo(e);
-    window.location.href - `${location}/2023-02-10/${interval}`
+let buttons = document.getElementsByTagName('button');
+let buttonCount = buttons.length;
+// buttons.addEventListener("onclick", function(e) {
+//     getInfo(e, location, interval);
+//     window.location.href - `${location}/2023-02-10/${interval}`
+// })
+for ( )
+buttons.addEventListener("click", function() {
+    let selector, location, interval;
+    selector = buttons.
 })
+
+// TODO:
+// For each button in buttons, add an eventlistener that
+// gets the selector next to that button, gets the location id 
+//  from its parent div, and gets the interval from the selector 
+// value. Also maybe make a date variable that holds 2/10/23
+// for now. Will eventually make it the current date
