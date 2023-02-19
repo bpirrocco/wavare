cf17bb52df0122b983a84df4958530f3b04b7e1f
Brandon Pirrocco <bpirrocco@gmail.com>
2023-02-18 19:32:26 -0500

    Add location value programmatically

--- home/static/home/script.js
+++ home/static/home/script.js
@@ -1,8 +1,25 @@
-let nazare = document.querySelector('#Nazaré');
-let nazButton = document.querySelector('#Nazarébutton');
+// let nazare = document.querySelector('#Nazaré');
+// let nazButton = document.querySelector('#Nazarébutton');
 
 
-nazButton.addEventListener("click", function() {
-    nazare = nazare.value;
-    window.location.href = `1/2023-02-10/${nazare}`
+// nazButton.addEventListener("click", function() {
+//     nazare = nazare.value;
+//     window.location.href = `1/2023-02-10/${nazare}`
+// })
+
+let location, interval;
+
+function getInfo(e) {
+    let button, selector;
+    button = e.target;
+    selector = button.previousSibling;
+    location = selector.id;
+    interval = selector.value;
+}
+
+let buttons = document.querySelector('button');
+
+buttons.addEventListener("click", function(e) {
+    getInfo(e);
+    window.location.href - `${location}/2023-02-10/${interval}`
 })
