2ecd85f917856b1389856bc9a46656aa33fc1935
Brandon Pirrocco <bpirrocco@gmail.com>
2023-02-20 21:13:32 -0500

    Add delay for second slideshow

--- home/static/home/script.js
+++ home/static/home/script.js
@@ -67,8 +67,15 @@ if (!buttons) {
 
 let slideIndex1 = 0;
 let slideIndex2 = 0;
+let slides2 = document.getElementsByClassName("myslide2");
+let j;
+for (j = 1; j <slides2.length; j++) {
+    slides2[j].style.display = "none";
+}
 showSlides();
-showSlides2();
+window.onload = function() {
+    setTimeout(showSlides2, 4000);
+}
 
 function showSlides() {
   let i;
@@ -85,6 +92,8 @@ function showSlides() {
 function showSlides2() {
     let i;
     let slides = document.getElementsByClassName("myslide2");
+    slides[0].classList.replace("invisible", "fade3");
+    // slides[0].classList.add("fade3");
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
     }
