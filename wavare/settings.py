daeb8522bfcd3a3b546961f806695157f370a530
Brandon Pirrocco <bpirrocco@gmail.com>
2023-03-09 19:34:15 -0500

    Enable logging

--- wavare/settings.py
+++ wavare/settings.py
@@ -134,3 +134,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
