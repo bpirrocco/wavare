f9ce53a601ac165a04dae7f725d9097cd9cc1c28
Brandon Pirrocco <bpirrocco@gmail.com>
2023-03-09 19:17:22 -0500

    Resolved all dependencies

---
+++ home/management/commands/startjobs.py
@@ -0,0 +1,16 @@
+import logging
+
+# Django
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+# Third Party
+import feedparser
+from dateutil import parser
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+
+# Models
+from home.models import Location, Forecast
