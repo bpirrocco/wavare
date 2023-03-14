5b77dfb29c34e370ea97a9aedce9f5c36aa2e556
Brandon Pirrocco <bpirrocco@gmail.com>
2023-03-14 16:35:54 -0400

    Getting rid of unneeded view

--- wavare/urls.py
+++ wavare/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, register_converter
 
-from home.views import welcome, nazare, forecast, forecasts, contact, about
+from home.views import welcome, forecast, forecasts, contact, about
 from home import converters
 
 register_converter(converters.DateConverter, 'date')
@@ -24,7 +24,6 @@ register_converter(converters.DateConverter, 'date')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome, name="home"),
-    path('nazare', nazare, name="nazare"),
     path('forecast/<int:location>/<date:date>/<str:interval>', forecast, name="forecast"),
     path('forecast/', forecasts, name="forecasts"),
     # re_path(r'^forecast/$', QuerystringRedirect.as_view(), name="forecaster"),
@@ -33,6 +32,3 @@ urlpatterns = [
     path('about', about, name="about"),
 ]
 
-
-# I need to make views for daily and today handling the data from the hourly forecast
-# to generate the data for the daily and today forecasts
