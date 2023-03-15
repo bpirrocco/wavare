02dc11a80f2e458e76a7517966f3e428712c7c8f
Brandon Pirrocco <bpirrocco@gmail.com>
2023-03-15 19:34:43 -0400

    Add some receivers for Forecast to clean up files

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
-from datetime import time
+import os
 
 from django.db import models
+from django.dispatch import receiver
 
 
 class Location(models.Model):
@@ -25,12 +26,35 @@ class Forecast(models.Model):
     def __str__(self):
         return f"Forecast for max wave height - {self.interval} in {self.location} on {self.date}"
 
-class ForecastTest(models.Model):
-    location =  models.ForeignKey(Location, on_delete = models.CASCADE)
-    date = models.DateField()
-    interval = models.CharField(max_length = 6, choices = INTERVAL_CHOICES, default = 'today')
-    filename = models.CharField(max_length = 100)
+@receiver(models.signals.post_delete, sender=Forecast)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """ Deletes file from filesystem
 
-    def __str__(self):
-        return f"Forecast for max wave height - {self.interval} in {self.location} on {self.date}"
+    when corresponding `Forecast` object is deleted.
 
+    """
+    if instance.file:
+        if os.path.isfile(instance.file.path):
+            os.remove(instance.file.path)
+
+@receiver(models.signals.pre_save, sender=Forecast)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """Deletes old file from filesystem
+
+    when corresponding `MediaFile` object is updated
+
+    with new file.
+
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = Forecast.objects.get(pk=instance.pk).file
+    except Forecast.DoesNotExist:
+        return False
+
+    new_file = instance.file
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
