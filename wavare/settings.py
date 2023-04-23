d5d80e84d457692daafc9c5eb3459ea9852224b7
Brandon Pirrocco <bpirrocco@gmail.com>
2023-04-23 16:32:59 -0400

    eb domain config

--- wavare/settings.py
+++ wavare/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["wavare.bpirrocco.dev"]
+ALLOWED_HOSTS = ["wavare.bpirrocco.dev",
+                 "wavare-v1-env.eba-7nc2cbn7.us-east-1.elasticbeanstalk.com",]
 
 
 # Application definition
