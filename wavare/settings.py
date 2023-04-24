8107bf9a29d5c2f708550ad4d6f99a9aff82a1f0
Brandon Pirrocco <bpirrocco@gmail.com>
2023-04-23 21:52:25 -0400

    Making app work again

--- wavare/settings.py
+++ wavare/settings.py
@@ -68,10 +68,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["wavare.bpirrocco.dev",
-                 "wavare-v1-env.eba-7nc2cbn7.us-east-1.elasticbeanstalk.com",]
+                 "wavare-v1-env.eba-7nc2cbn7.us-east-1.elasticbeanstalk.com",
+                 "127.0.0.1"]
 private_ip = get_linux_ec2_private_ip()
 if private_ip:
    ALLOWED_HOSTS.append(private_ip)
@@ -183,8 +184,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
